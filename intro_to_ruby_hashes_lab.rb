7de022cd522037452c55a183d003e0279bf3cc30
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-03 23:25:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {
-      one_piece_owned: 25,
+    rent_in_dollars: {
+      :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
