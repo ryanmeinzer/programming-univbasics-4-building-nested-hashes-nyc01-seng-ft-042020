adc7b2475157abd3ffb5ec8b78f6521de3588635
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-03 23:20:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroads => {
-    pieces: 4
-    rent_in_dollars: {}
-    names: {}
+    :pieces => 4
+    :rent_in_dollars => {}
+    :names => {}
   }
   }
 end
